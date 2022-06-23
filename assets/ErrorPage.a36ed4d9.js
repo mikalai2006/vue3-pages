141cc1b370fc69cb56ccbefbd6a91369187e415d
mikalai2006 <composer.opencart@gmail.com>
2022-06-23 19:53:54 +0300

    deploy

---
+++ assets/ErrorPage.a36ed4d9.js
@@ -0,0 +1,5 @@
+import{d as s,o as r,c as n,b as e,t,p as c}from"./index.fb91edd4.js";const p={class:"bg-white shadow"},d={class:"max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"},l={class:"text-3xl font-bold leading-tight text-gray-900"},i=c(" ErrorPage "),x=s({__name:"ErrorPage",props:{error:Object},setup(o){return(a,_)=>(r(),n("header",p,[e("div",d,[e("h1",l,[i,e("pre",null,`          Route name and path
+          `+t(a.$route.path)+`
+          /
+          `+t(o.error)+`
+        `,1)])])]))}});export{x as default};
